775996079164f4bdc76c2c8313ccbd2406cc2ff1
melvinheks <mcheria000@citymail.cuny.edu>
2018-01-27 12:09:48 -0500

    307 second version when wrong, this version will output a result larger than expected, as opposed to outputting a smaller result such as in the previous version.

--- 307-Sticks.cpp
+++ 307-Sticks.cpp
@@ -1,17 +1,45 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
+int* vals;
+
+bool* unadded;
+bool possible_length(int n, int total, int goal)
+{
+	if(total > goal) return false;
+	if(total == goal) return true;
+	for(int i = 0; i < n; i++){
+		if(unadded[i]){
+			unadded[i] = false;
+			if(possible_length(n, total+vals[i], goal)){
+				return true;	
+			}
+			else{
+				unadded[i] = true;
+			}
+		}
+	}
+	return false;
+
+}
 int main()
 {
     int n;
+	int size;
     int m;
     int sum = 0;
     int max = 0;
     while(cin >> n){
         if(n==0)break;
+		vals = new int[n];
+		unadded = new bool[n];
+		size = n;
+		fill_n(unadded, n, true);
         while(n-->0){
             cin >> m;
+			vals[n] = m;
             sum += m;
             if(m > max) max = m;
         }
@@ -20,12 +48,22 @@ int main()
             if(sum%temp==0 && sum/temp >= max){
                 //cout<<sum<<endl;
                 //cout<<temp<<endl;
-                cout<<sum/temp<<endl;
-                break;
+				int i;
+				for(i = 0; i < temp; i++){
+					if(!possible_length(size, 0, sum/temp)){
+						fill_n(unadded, size, true);
+						break;
+					}
+				}
+				if(i==temp){
+					cout<<sum/temp<<endl;
+					break;
+				}
             }
             temp--;
         }
         max = 0;
         sum = 0;
+		delete[] vals;
     }
 }
